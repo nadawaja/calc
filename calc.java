ad444207ea331f4212e024f5293a5dd475214dfa
nadawaja <21028149@student.ltuc.com>
2024-06-11 10:24:44 +0300

    added sqr power fac

--- calc.java
+++ calc.java
@@ -18,3 +18,24 @@ public class Calculator {
         return (double) a / b;
     }
 }
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public double sqrt(double number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Cannot compute square root of a negative number.");
+        }
+        return Math.sqrt(number);
+    }
+
+    public long factorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Cannot compute factorial of a negative number.");
+        }
+        long result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
